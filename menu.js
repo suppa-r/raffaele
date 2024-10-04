4be47368876e6649b63bea833dbb3fa0514babb3
suppa-r <supparaffaele43@gmail.com>
2024-10-03 23:33:17 -0400

    aaa

--- menu.js
+++ menu.js
@@ -1,22 +1,48 @@
-var menuItems = document.querySelectorAll("li.has-submenu");
-Array.prototype.forEach.call(menuItems, function (el, i) {
-  var activatingA = el.querySelector("a");
-  var btn ='show submenu for “' + activatingA.text + "”";
-  activatingA.insertAdjacentHTML("afterend", btn);
-
-  el.querySelector("button").addEventListener("click", function (event) {
-    if (this.parentNode.className == "has-submenu") {
-      this.parentNode.className = "has-submenu open";
-      this.parentNode.querySelector("a").setAttribute("aria-expanded", "true");
-      this.parentNode.querySelector("button").setAttribute("aria-expanded", "true");
-    } else {
-      this.parentNode.className = "has-submenu";
-      this.parentNode.querySelector("a").setAttribute("aria-expanded", "false");
-      this.parentNode.querySelector("button").setAttribute("aria-expanded", "false");
-    }
-    event.preventDefault();
-  });
+var menuItems = document.querySelectorAll('li.has-submenu');
+Array.prototype.forEach.call(menuItems, function(el, i){
+	el.addEventListener("mouseover", function(event){
+		this.className = "has-submenu open";
+		clearTimeout(timer);
+	});
+	el.addEventListener("mouseout", function(event){
+		timer = setTimeout(function(event){
+			document.querySelector(".has-submenu.open").className = "has-submenu";
+		}, 1000);
+	});
 });
+
+
+var menuItems = document.querySelectorAll('li.has-submenu');
+Array.prototype.forEach.call(menuItems, function(el, i){
+	el.querySelector('a').addEventListener("click",  function(event){
+		if (this.parentNode.className == "has-submenu") {
+			this.parentNode.className = "has-submenu open";
+			this.setAttribute('aria-expanded', "true");
+		} else {
+			this.parentNode.className = "has-submenu";
+			this.setAttribute('aria-expanded', "false");
+		}
+		event.preventDefault();
+		return false;
+	});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
