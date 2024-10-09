b3c9ed8368c73ca34de5aaec7a3ddc7b7e317a55
suppa-r <supparaffaele43@gmail.com>
2024-10-08 21:29:16 -0400

    aaaaa

---
+++ nav.js
@@ -0,0 +1,6 @@
+const resizeBtn = document.querySelector("[data-resize-btn]");
+  
+  resizeBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.body.classList.toggle("sb-expanded");
+  });
